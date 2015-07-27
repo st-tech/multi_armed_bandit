0d9a58fa21eb82d48b358f771269c658d670ff0f
kndt84 <takashi.kaneda@vasily.jp>
2015-07-27 23:56:24 +0900

    add spec tests

--- lib/multi_armed_bandit/softmax.rb
+++ lib/multi_armed_bandit/softmax.rb
@@ -1,33 +1,40 @@
-class Softmax
 
-  def initialize(temperature, n_arms)
-    @temperature = temperature
-    @counts = Array.new(n_arms, 0)
-    @values = Array.new(n_arms, 0.0)
-  end
 
-  def reset(n_arms)
-    @counts = Array.new(n_arms, 0)
-    @values = Array.new(n_arms, 0.0)
-  end
+module MultiArmedBandit
 
-  def update(new_counts, new_rewards)
+  class Softmax
 
-    # update expectations of each arm
-    new_values = []
-    @counts.zip(@values, new_counts, new_rewards).each do |n, value, nn, reward|
-      new_values << (n * value + reward) / (n + nn)
+    attr_accessor :temperature, :counts, :values, :probs
+
+    def initialize(temperature, n_arms)
+      @temperature = temperature
+      reset(n_arms)    
     end
-    @values = new_values
 
-    # update the numbers of each arm's trial
-    @counts = @counts.zip(new_counts).map{|f, s| f + s}
+    def reset(n_arms)
+      @counts = Array.new(n_arms, 0)
+      @values = Array.new(n_arms, 0.0)
+      @probs = Array.new(n_arms, 0.0)
+    end
 
-    # calcurate probabilities
-    z = @values.collect{|i| Math.exp(i/@temperature)}.reduce(:+)
-    probs = @values.collect{|i| Math.exp(i/@temperature)/z}
+    def bulk_update(new_counts, new_rewards)
 
-    return probs
+      # update expectations of each arm
+      new_values = []
+      @counts.zip(@values, new_counts, new_rewards).each do |n, value, nn, reward|
+        new_values << (n * value + reward) / (n + nn)
+      end
+      @values = new_values
+
+      # update the numbers of each arm's trial
+      @counts = @counts.zip(new_counts).map{|f, s| f + s}
+
+      # calcurate probabilities
+      z = @values.collect{|i| Math.exp(i/@temperature)}.reduce(:+)
+      @probs = @values.collect{|i| Math.exp(i/@temperature)/z}
+
+      return probs
+    end
 
   end
 end
--- spec/multi_armed_bandit_spec.rb
+++ spec/multi_armed_bandit_spec.rb
@@ -4,8 +4,32 @@ describe MultiArmedBandit do
   it 'has a version number' do
     expect(MultiArmedBandit::VERSION).not_to be nil
   end
+ end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
-end
+describe MultiArmedBandit::Softmax do
+
+  counts = [100,100,100,100,100]
+  rewards = [1,1,1,1,1]
+
+  it 'sum of prob list equals 1.0' do
+ 	sm = MultiArmedBandit::Softmax.new(0.01, 5)
+ 	probs = sm.bulk_update(counts, rewards)
+ 	# round 15 decimal places
+ 	sum = probs.inject(:+).round(15)
+ 	expect(sum).to eq(1.0)
+  end	
+
+  it 'reset method works' do
+ 	sm = MultiArmedBandit::Softmax.new(0.01, 3)
+ 	sm.bulk_update(counts, rewards)
+ 	sm.reset(3)
+ 	expect(sm.counts).to eq([0,0,0])
+  end	
+
+  it 'probs return exact numbers' do
+ 	sm = MultiArmedBandit::Softmax.new(0.01, 5)
+ 	probs = sm.bulk_update(counts, [1,1,1,1,1])
+ 	expect(probs).to eq([0.2,0.2,0.2,0.2,0.2])
+  end	
+
+end 
