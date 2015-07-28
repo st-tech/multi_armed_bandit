ae0bf2ab3351809fb1a7bc356e044195cc94da6f
kndt84 <takashi.kaneda@vasily.jp>
2015-07-28 18:00:58 +0900

    add update method

--- lib/multi_armed_bandit/softmax.rb
+++ lib/multi_armed_bandit/softmax.rb
@@ -6,17 +6,21 @@ module MultiArmedBandit
 
     attr_accessor :temperature, :counts, :values, :probs
 
+    # Initialize an object
     def initialize(temperature, n_arms)
       @temperature = temperature
       reset(n_arms)    
     end
 
+    # Reset instance variables
     def reset(n_arms)
       @counts = Array.new(n_arms, 0)
       @values = Array.new(n_arms, 0.0)
       @probs = Array.new(n_arms, 0.0)
     end
 
+    # Update in a lump. new_counts is a list of each arm's trial number and
+    # new_rewards means a list of rewards.
     def bulk_update(new_counts, new_rewards)
 
       # update expectations of each arm
@@ -36,5 +40,39 @@ module MultiArmedBandit
       return probs
     end
 
+    
+    def update(chosen_arm, reward)
+      @counts[chosen_arm] = @counts[chosen_arm] + 1
+      n = @counts[chosen_arm]
+      
+      value = @values[chosen_arm]
+      new_value = ((n - 1) / n.to_f) * value + (1 / n.to_f) * reward
+      @values[chosen_arm] = new_value
+      return
+    end
+      
+    
+    def select_arm
+      z = @values.collect{|i| Math.exp(i/@temperature)}.reduce(:+)
+      @probs = @values.collect{|i| Map.exp(i/@temperature)/z}
+      return categorical_draw(@probs)
+    end
+
+    private
+    def categorical_draw(probs)
+      z = rand()
+      cum_prob = 0.0
+
+      probs.size().times do |i|
+        prob = probs[i]
+        cum_prob += prob
+        if cum_prob > z
+          return i
+        end
+      end
+      
+      return probs.size() - 1
+    end
+    
   end
 end
