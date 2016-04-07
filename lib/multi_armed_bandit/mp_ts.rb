51ac6e80ce60a47a2d292eb38906a4b648342fc5
kndt84 <takashi.kaneda@vasily.jp>
2016-04-07 21:40:07 +0900

    add functions

--- lib/multi_armed_bandit/mp_ts.rb
+++ lib/multi_armed_bandit/mp_ts.rb
@@ -4,15 +4,14 @@ module MultiArmedBandit
 
   class MultiplePlayTS
 
-    attr_accessor :k, :l, :r, :alpha, :beta
+    attr_accessor :n_arms, :n_selected_arms, :r, :alpha, :beta
 
     # Initialize an object
     # k: num of arms
     # l: num of selected arms
     def initialize(k, l)
-      @k  = k
-      @l = l
-      @r = SimpleRandom.new
+      @n_arms  = n_arms
+      @n_selected_arms = n_selected_arms
       reset
     end
 
@@ -20,14 +19,24 @@ module MultiArmedBandit
     def reset
       @alpha = Array.new(@n_arms, 1)
       @beta = Array.new(@n_arms, 1)
+      @r = SimpleRandom.new
     end
 
-    def get_select_arms(alpha, beta)
+    def get_select_arms
       selected_arms = @alpha.zip(@beta).each_with_index
                       .map{|c,i| [i, @r.beta(c[0],c[1]) ] }
                       .sort_by{ |v| -s[1]}.
-                      .map{|v| v[0]}[0..l-1]
+                      .map{|v| v[0]}[0..n_selected_arms-1]
+      update_disp_count(selected_arms)
       return selected_arms
+    end
+
+    def add_disp_count(selected_arms)
+      selected_arms.each{|i| @beta += 1}
+    end
+
+    def add_click_count(idx)
+      @alpha[idx] += 1
     end
 
   end
